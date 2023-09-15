6a02ada0775ba67ce597e887f238ec2ff7d46a98
Jasmine <tanghocle456@gmail.com>
2023-09-14 22:53:20 -0700

    Add basic tokens

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,8 +1,20 @@
+#[derive(Debug)]
 pub enum TOKEN {
-    ADD,
-    SUB,
-    MUL,
-    DIV,
+    // BASIC MATH OPERATION
+    ADD(char),
+    SUB(char),
+    MUL(char),
+    DIV(char),
+    MOD(char),
+
+    // EXP AND FloorDiv
+    EXP(str),
+    FloorDiv(str),
+
+    // COMMENTS
+    SingleComment(str),
+    BlockComment(str),
+
 }
 pub fn say_hello() {
     println!("Hello World");
