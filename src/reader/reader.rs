c6755ab604b511334b1f340004980853b02b7b9d
Jasmine T <tanghocle456@gmail.com>
2023-09-17 15:19:56 -0700

    Add two test cases for read file

--- src/reader/reader.rs
+++ src/reader/reader.rs
@@ -1,36 +1,37 @@
-#[derive(Debug)]
-pub enum TOKEN {
-    // BASIC MATH OPERATION
-    ADD(char),
-    SUB(char),
-    MUL(char),
-    DIV(char),
-    MOD(char),
+use std::env;
+use std::fs;
 
-    // EXP AND FloorDiv
-    EXP(Vec<char>),
-    FloorDiv(Vec<char>),
+pub struct Reader {}
 
-    // COMMENTS
-    SingleComment(Vec<char>),
-    BlockComment(Vec<char>),
+impl Reader {
+    pub fn read_file(file_path : &str) -> String {
+        println!("In file {}", file_path);
 
-}
+        let error_msg = format!("Should have been able to read the {}", file_path);
+        let contents = fs::read_to_string(file_path)
+            .expect(error_msg.as_str());
 
-pub struct Lexer {
-    input: Vec<char>,           // Source code
-    pub position: usize,        // Reading position
-    pub read_position: usize,   // Current moving reading position
-    pub ch: char                // Current read character
-}
-
-impl Lexer {
-    fn new() {}             // Create a new Lexer instance
-    pub fn read_char() {}   // Read next char, update positions
-    pub fn next_token() {}  // Match the read character and assign appropriate type
+        contents
+    }
 }
 
 
-pub fn say_hello() {
-    println!("Hello World");
+#[cfg(test)]
+mod test_reader {
+    use crate::reader::reader::Reader;
+
+    const TEST_MATERIAL_DIRECTORY: &str = "src/reader/test_material/read_file";
+    #[test]
+    fn test_read_file() {
+        let mut file_path = format!("{}/test_read_file.txt", TEST_MATERIAL_DIRECTORY);
+        let mut result: String = String::from("I'm nobody! Who are you?");
+
+        let test_1_msg: &str = "Reading single line from file";
+        assert_eq!(Reader::read_file(&file_path), result, "{}", test_1_msg);
+
+        file_path = format!("{}/test_newline_twoline.txt", TEST_MATERIAL_DIRECTORY);
+        result = String::from("I'm nobody! Who are you?\n");
+        let test_2_msg: &str = "Reading two line from file, expected a newline char";
+        assert_eq!(Reader::read_file(&file_path), result, "{}", test_2_msg);
+    }
 }
