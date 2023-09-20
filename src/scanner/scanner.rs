fa8aa6c0a26422d1f96afeaf44863d62ad7a85eb
Jasmine T <tanghocle456@gmail.com>
2023-09-20 11:31:31 -0700

    Add constructor and tokens for lexer

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,32 +1,63 @@
+use crate::reader::reader::{Reader, ReaderGeneral};
 
-
-
+// In case of multiple readers implementation, please use the next line to set the reader
+// type Reader = Reader
 #[derive(Debug)]
 pub enum TOKEN {
     // BASIC MATH OPERATION
-    ADD(char),
-    SUB(char),
-    MUL(char),
-    DIV(char),
-    MOD(char),
+    ADD, // +
+    SUB,  // -
+    MUL, // *
+    DIV, // /
+    MOD, // %
+
+    AND, // &&
+    OR, // ||
+    XOR, // ^
+    SHL, // <<
+    SHR, // >>
+
+    EQUAL,    // ==
+    LESS,   // <
+    GREATER,   // >
+    ASSIGN, // =
+    NOT,   // !
+
+
+    AndAssign, // &=
+    OrAssign, // !=
+    XorAssign, // ^=
+    ShlAssign, // <<=
+    ShrAssign, // >>=
+    AndNotAssign, // &^=
 
     // EXP AND FloorDiv
-    EXP(Vec<char>),
-    FloorDiv(Vec<char>),
+    EXP, // **
+    FloorDiv, // /_
+    CeilDiv, // /^
 
     // COMMENTS
-    SingleComment(Vec<char>),
-    BlockComment(Vec<char>),
+    SingleComment, //
+    // BlockComment(Vec<char>),
+
 
 }
 
 pub struct Lexer {
-
+    reader : Reader,
 }
 
 impl Lexer {
-    pub fn new() {}             // Create a new Lexer instance
-    pub fn next_token() {}  // Match the read character and assign appropriate type
+    // Create a new Lexer instance
+    pub fn new(file_path : &str) -> Self {
+        Self {
+            reader : Reader::new(file_path)
+        }
+    }
+    pub fn next_token() {
+
+    }  // Match the read character and assign appropriate type
+
 }
 
 
