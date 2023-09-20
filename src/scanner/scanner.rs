10b00f16f07c1777a4f00ef65bde5afe222de703
Jasmine T <tanghocle456@gmail.com>
2023-09-20 11:43:43 -0700

    Working on next_token

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,4 +1,5 @@
 use crate::reader::reader::{Reader, ReaderGeneral};
+use crate::scanner::token::TOKEN;
 
 // In case of multiple readers implementation, please use the next line to set the reader
 // type Reader = Reader
@@ -14,8 +15,12 @@ impl Lexer {
             reader : Reader::new(file_path)
         }
     }
-    pub fn next_token() {
+    pub fn next_token(&self) -> TOKEN {
 
+        while !self.reader.is_eof() {
+
+        }
+        return TOKEN::ADD
     }  // Match the read character and assign appropriate type
 
 }
