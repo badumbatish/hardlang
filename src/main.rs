f3b6c84b13c320cab12265dd3c67a7414ebd08e7
Jasmine T <tanghocle456@gmail.com>
2024-01-29 16:44:05 -0800

    Messing around with AstNode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use crate::parser::ast::{AstNode, AstNodeRef};
+use crate::scanner::token::TOKEN;
+
 mod parser;
 mod reader;
 mod scanner;
 
-fn main() {}
+fn main() {
+    let a = TOKEN::NUM(64.0);
+
+    let b : TOKEN = TOKEN::ADD;
+    let c : Option<AstNodeRef> = None;
+    let d : Option<AstNodeRef> = None;
+    let e : TOKEN = TOKEN::NUM(0.0);
+
+    let ast : AstNode = AstNode { op: b, left: c, right: d, value: e };
+}
