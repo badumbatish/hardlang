a514412566110fb30660d3c7ea4e562fcad7b760
jjasmine <tanghocle456@gmail.com>
2024-03-24 14:03:10 -0400

    Reset

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
-use crate::parser::ast::{AstNode, AstNodeRef};
-use crate::scanner::token::TOKEN;
-
-mod parser;
-mod reader;
-mod scanner;
 
 fn main() {
-    let _a = TOKEN::NUM("0.0".to_string());
-
-    let b : TOKEN = TOKEN::ADD;
-    let c : Option<AstNodeRef> = None;
-    let d : Option<AstNodeRef> = None;
-    let e : TOKEN = TOKEN::NUM("0.0".to_string());
-
-    let _ast : AstNode = AstNode { op: b, left: c, right: d, value: e };
+    println!("Hello, world!");
 }
--- src/parser/parser.rs
+++
@@ -1,8 +0,0 @@
-/// The parser class for our language
-/// It will have
-/// get_next_token
-/// make_ast_tree
-/// traverse_ast_tree
-pub struct Parser {
-    // TODO: Given a bunch of tokens, how do I make an AST?
-}
